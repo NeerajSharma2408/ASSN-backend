68e4f2d4d0c543c8e6c514d3c902ec35baa95823
Neeraj Sharma <neesharm24@gmail.com>
2024-04-06 14:15:01 +0530

    changes reverted

--- utils/bcryptHash.js
+++ utils/bcryptHash.js
@@ -1,31 +1,35 @@
 const bcrypt = require('bcrypt');
-const saltRounds = Number(process.env.SALT_ROUNDS) || 10;
+const saltRounds = Number(process.env.SALT_ROUNDS) || 2;
 
 const getHash = async (plaintextPassword) => {
-    // return new Promise((resolve, reject)=>{
-    // bcrypt.hash(plaintextPassword, saltRounds, function(err, hash) {
-    //     if(err){
-    //         reject(`INTERNAL HASHING ERROR: ${err}`)
-    //     }
-    //     resolve(hash?.toString());
-    //     console.log(hash?.toString())
-    // });});
+    // ASYNC APPROACH
+    return new Promise((resolve, reject)=>{
+    bcrypt.hash(plaintextPassword, saltRounds, function(err, hash) {
+        if(err){
+            reject(`INTERNAL HASHING ERROR: ${err}`)
+        }
+        resolve(hash?.toString());
+        console.log(hash?.toString())
+    });});
 
-    const hash = bcrypt.hashSync(plaintextPassword, saltRounds);
-    return (hash).toString() ?? new Error(`INTERNAL HASHING ERROR: ${hash}`) 
+    // SYNC APPROACH
+    // const hash = bcrypt.hashSync(plaintextPassword, saltRounds);
+    // return (hash).toString() ?? new Error(`INTERNAL HASHING ERROR: ${hash}`) 
 };
 
 const verifyHash = async (hashPassword, plaintextPassword) => {
-    // return new Promise((resolve, reject)=>{
-    // bcrypt.compare(plaintextPassword, hashPassword, function(err, result) {
-    //     if(err){
-    //         reject(`INTERNAL HASHING ERROR: ${err}`)
-    //     }
-    //     resolve(result);
-    // });});
+    // ASYNC APPROACH
+    return new Promise((resolve, reject)=>{
+    bcrypt.compare(plaintextPassword, hashPassword, function(err, result) {
+        if(err){
+            reject(`INTERNAL HASHING ERROR: ${err}`)
+        }
+        resolve(result);
+    });});
 
-    const result = bcrypt.compareSync(plaintextPassword, hashPassword);
-    return result ?? new Error(`INTERNAL HASHING ERROR: ${hash}`); 
+    // SYNC APPROACH
+    // const result = bcrypt.compareSync(plaintextPassword, hashPassword);
+    // return result ?? new Error(`INTERNAL HASHING ERROR: ${hash}`); 
 }
 
 module.exports = { getHash, verifyHash }
