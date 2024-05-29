c3a0386a951f09c8bee2243866ba02d39a93c760
Neeraj Sharma <neesharm24@gmail.com>
2024-05-29 19:18:17 +0530

    error handled for socket driven notification controllers

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -7,151 +7,163 @@ const Comment = require("../model/Comment");
 const Post = require("../model/Post");
 const Message = require("../model/Message");
 
-const getAllNotification = expressAsyncHandler( async (req, res)=>{
+const getAllNotification = expressAsyncHandler(async (req, res) => {
 
     const selfId = res.locals.id;
     const limit = req.query.limit ?? 15;
     const page = req.query.page ?? 1;
 
-    const allNotifications = await Notification.find({To: selfId, isDeleted: false});
+    const allNotifications = await Notification.find({ To: selfId, isDeleted: false });
 
-    res.status(200).json({message: "ALL NOTIFICATIONS Fetched", allNotifications}).skip((page - 1) * limit).limit(limit).exec();
+    res.status(200).json({ message: "ALL NOTIFICATIONS Fetched", allNotifications }).skip((page - 1) * limit).limit(limit).exec();
 });
 
-const deleteAllNotification = expressAsyncHandler( async (req, res)=>{
+const deleteAllNotification = expressAsyncHandler(async (req, res) => {
 
     const selfId = res.locals.id;
 
-    const allNotifications = await Notification.updateMany({To: selfId, isDeleted: false}, {isDeleted: true});
-    res.status(200).json({message: "ALL NOTIFICATIONS DELETED", allNotifications})
+    const allNotifications = await Notification.updateMany({ To: selfId, isDeleted: false }, { isDeleted: true });
+    res.status(200).json({ message: "ALL NOTIFICATIONS DELETED", allNotifications })
 });
 
-const deleteNotification = expressAsyncHandler( async (req, res)=>{
+const deleteNotification = expressAsyncHandler(async (req, res) => {
 
     const selfId = res.locals.id;
     const notificationId = req.params.notificationId;
 
-    if(!notificationId) throw new Error("notification Id is not Present")
+    if (!notificationId) throw new Error("notification Id is not Present")
 
     // mongoose.Types.ObjectId.isValid(any_id) --- to test if id is a valid object_id
     const notificationExists = await Notification.exists({ id: notificationId });
 
-    if(!notificationExists) {
+    if (!notificationExists) {
         res.status(404)
         throw new Error("Notification doesn't exists for given Id")
     }
 
-    const deletedNotification = await Notification.findByIdAndUpdate(notificationId, {isDeleted: true});
-    res.status(200).json({message: "NOTIFICATION DELETED", deletedNotification})
+    const deletedNotification = await Notification.findByIdAndUpdate(notificationId, { isDeleted: true });
+    res.status(200).json({ message: "NOTIFICATION DELETED", deletedNotification })
 });
 
 // SOCKET CONTROLLERS
-const sendCommentNotification = expressAsyncHandler( async (io, userID, comment, postBy)=>{
+const sendCommentNotification = expressAsyncHandler(async (io, userID, comment, postBy) => {
+    try {
 
-    if(comment?.By === userID || !io) return;
+        if (comment?.By === userID || !io) return;
 
-    const isReply = comment?.Parent ? true : false;
-    const user = await User.findById(userID).select('Name Username id Avatar');
-    let message = `${user.Username} has Commented on your Post`;
-    
-    let notificationObj = {
-        From: userID,
-        To: postBy,
-        Type: 3,
-        RefObject: {
-            RefSchema: 'Comment',
-            RefId: comment.id
-        },
-        message
+        const isReply = comment?.Parent ? true : false;
+        const user = await User.findById(userID).select('Name Username id Avatar');
+        let message = `${user.Username} has Commented on your Post`;
+
+        let notificationObj = {
+            From: userID,
+            To: postBy,
+            Type: 3,
+            RefObject: {
+                RefSchema: 'Comment',
+                RefId: comment.id
+            },
+            message
+        }
+        let postNotification = await Notification.create(notificationObj);
+
+        io.to(ConnectedUsers[postBy]?.socketId).emit('post-comment-notification', { comment, user, postNotification });
+
+        if (!isReply) return;
+
+        message = `${user.Username} has replied to your Comment`;
+        let parentComment = await Comment.findById(comment?.Parent);
+
+        notificationObj = {
+            From: userID,
+            To: parentComment?.By,
+            Type: 4,
+            RefObject: {
+                RefSchema: 'Comment',
+                RefId: comment.id
+            },
+            message
+        }
+        let ReplyNotification = await Notification.create(notificationObj);
+
+        io.to(ConnectedUsers[parentComment?.By]?.socketId).emit('comment-reply-notification', { comment, user, ReplyNotification });
+    } catch (error) {
+        console.log(error);
     }
-    let postNotification = await Notification.create(notificationObj);
-
-    io.to(ConnectedUsers[postBy]?.socketId).emit('post-comment-notification', { comment, user, postNotification });
-
-    if(!isReply) return;
-
-    message = `${user.Username} has replied to your Comment`;
-    let parentComment = await Comment.findById(comment?.Parent);
-
-    notificationObj = {
-        From: userID,
-        To: parentComment?.By,
-        Type: 4,
-        RefObject: {
-            RefSchema: 'Comment',
-            RefId: comment.id
-        },
-        message
-    }
-    let ReplyNotification = await Notification.create(notificationObj);
-
-    io.to(ConnectedUsers[parentComment?.By]?.socketId).emit('comment-reply-notification', { comment, user, ReplyNotification });
 });
 
-const sendReactionNotification = expressAsyncHandler( async (io, userID, reaction, ref, toID)=>{
+const sendReactionNotification = expressAsyncHandler(async (io, userID, reaction, ref, toID) => {
+    try {
 
-    if(reaction?.By === userID || !io) return;
+        if (reaction?.By === userID || !io) return;
 
-    const user = await User.findById(userID).select('Name Username id Avatar');
-    let message = `${user.Username} has Reacted to your ${ref}`;
-    
-    let parent = null;
-    let parentId = null;
-    switch (ref) {
-        case 'Post':
-            parent = await Post.findById(comment?.Parent);
-            parentId = parent?.By;
-            break;
-        case 'Comment':
-            parent = await Comment.findById(comment?.Parent);
-            parentId = parent?.By;
-            break;
-        case 'Message':
-            parent = await Message.findById(comment?.Parent);
-            parentId = parent?.Sender;
-            break;
-        default:
-            break;
+        const user = await User.findById(userID).select('Name Username id Avatar');
+        let message = `${user.Username} has Reacted to your ${ref}`;
+
+        let parent = null;
+        let parentId = null;
+        switch (ref) {
+            case 'Post':
+                parent = await Post.findById(comment?.Parent);
+                parentId = parent?.By;
+                break;
+            case 'Comment':
+                parent = await Comment.findById(comment?.Parent);
+                parentId = parent?.By;
+                break;
+            case 'Message':
+                parent = await Message.findById(comment?.Parent);
+                parentId = parent?.Sender;
+                break;
+            default:
+                break;
+        }
+
+        let notificationObj = {
+            From: userID,
+            To: parentId,
+            Type: ref === 'Post' ? 5 : (ref === "Comment" ? 6 : 7),
+            RefObject: {
+                RefSchema: 'Reaction',
+                RefId: reaction.id
+            },
+            message
+        }
+        let ReactNotification = await Notification.create(notificationObj);
+
+        io.to(ConnectedUsers[toID]?.socketId).emit('reaction-notification', { reaction, user, ref, ReactNotification });
+    } catch (error) {
+        console.log(error);
     }
-
-    let notificationObj = {
-        From: userID,
-        To: parentId,
-        Type: ref === 'Post' ? 5 : (ref === "Comment" ? 6 : 7),
-        RefObject: {
-            RefSchema: 'Reaction',
-            RefId: reaction.id
-        },
-        message
-    }
-    let ReactNotification = await Notification.create(notificationObj);
-
-    io.to(ConnectedUsers[toID]?.socketId).emit('reaction-notification', { reaction, user, ref, ReactNotification });
 });
 
-const sendRequestNotification = expressAsyncHandler( async (io, userID, request, toID, type) => {
-    if(request?.Recipient === userID || !io) return;
+const sendRequestNotification = expressAsyncHandler(async (io, userID, request, toID, type) => {
+    try {
+        if (request?.Recipient === userID || !io) return;
 
-    const user = await User.findById(userID).select('Name Username id Avatar');
-    let message = type === 'accept' ? 
-        `${user.Username} has accepted your friend Request` :
-        type === 'add' ? 
-            `${user.Username} has sent you a friend Request` : 
-            `${user.Username} has declined your a friend Request`;
+        const user = await User.findById(userID).select('Name Username id Avatar');
+        let message = type === 'accept' ?
+            `${user.Username} has accepted your friend Request` :
+            type === 'add' ?
+                `${user.Username} has sent you a friend Request` :
+                `${user.Username} has declined your a friend Request`;
 
-    let notificationObj = {
-        From: userID,
-        To: request?.Recipient,
-        Type: type === 'accept' ? 1 : type === 'add' ? 0 : 2,
-        RefObject: {
-            RefSchema: 'Friend',
-            RefId: request.id
-        },
-        Message: message
+        let notificationObj = {
+            From: userID,
+            To: request?.Recipient,
+            Type: type === 'accept' ? 1 : type === 'add' ? 0 : 2,
+            RefObject: {
+                RefSchema: 'Friend',
+                RefId: request.id
+            },
+            Message: message
+        }
+        let RequestNotification = await Notification.create(notificationObj);
+
+        io.to(ConnectedUsers[toID]?.socketId).emit('request-notification', { request, user, RequestNotification });
+    } catch (error) {
+        console.log(error);
     }
-    let RequestNotification = await Notification.create(notificationObj);
-
-    io.to(ConnectedUsers[toID]?.socketId).emit('request-notification', { request, user, RequestNotification });
 });
 
 module.exports = { getAllNotification, deleteAllNotification, deleteNotification, sendCommentNotification, sendReactionNotification, sendRequestNotification }
